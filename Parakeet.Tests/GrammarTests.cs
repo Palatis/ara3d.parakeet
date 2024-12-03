b6d52c1e3001c037673c47c6ad66eb9275ce4ff3
Victor Tseng <palatis@gmail.com>
2024-12-03 19:43:40 +0800

    [Tests] GrammarTests.OptimizerTest()
    test against grammar and rule with the RuleOptimizer
    
    reformat the output so it's more human-readable.

--- Parakeet.Tests/GrammarTests.cs
+++ Parakeet.Tests/GrammarTests.cs
@@ -10,19 +10,35 @@ public static class GrammarTests
         => AllGrammars.Grammars;
 
     [Test, TestCaseSource(nameof(Grammars))]
-    public static void OptimizerTest(Grammar g)
+    public static void OptimizerOnGrammarTest(Grammar g)
     {
-
-        Console.WriteLine("Rule Optimization");
         var ro = new RuleOptimizer(g);
 
+        Console.WriteLine($"Optimized Rules (Count = {ro.OptimizedRules.Count}):");
         foreach (var kv in ro.OptimizedRules)
         {
-            Console.WriteLine("Original");
-            Console.WriteLine(kv.Key);
+            Console.Write($"  From ({kv.Key.GetType().Name}): ");
+            Console.WriteLine(kv.Key.ToString().Replace("\r", "\\r").Replace("\n", "\\n"));
 
-            Console.WriteLine("Optimized");
-            Console.WriteLine(kv.Value);
+            Console.Write($"  Into ({kv.Value.GetType().Name}): ");
+            Console.WriteLine(kv.Value.ToString().Replace("\r", "\\r").Replace("\n", "\\n"));
+        }
+    }
+
+    [Test, TestCaseSource(nameof(Grammars))]
+    public static void OptimizerOnRuleTest(Grammar g)
+    {
+        var ro = new RuleOptimizer();
+        _ = ro.Optimize(g.StartRule);
+
+        Console.WriteLine($"Optimized Rules (Count = {ro.OptimizedRules.Count}):");
+        foreach (var kv in ro.OptimizedRules)
+        {
+            Console.Write($"  From ({kv.Key.GetType().Name}): ");
+            Console.WriteLine(kv.Key.ToString().Replace("\r", "\\r").Replace("\n", "\\n"));
+
+            Console.Write($"  Into ({kv.Value.GetType().Name}): ");
+            Console.WriteLine(kv.Value.ToString().Replace("\r", "\\r").Replace("\n", "\\n"));
         }
     }
 
