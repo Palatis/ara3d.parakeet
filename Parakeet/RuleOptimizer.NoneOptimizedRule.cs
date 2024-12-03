dcb7bbc2bc156435d66414078fa0d8b464648aaf
Victor Tseng <palatis@gmail.com>
2024-12-03 19:15:35 +0800

    [optimizer] optimize `RecursiveRule`
    allow `RuleOptimizer` to optimize `RecursiveRule`.
    
    steps:
    1. add a holder class to the LUT, indicates that this rule haven't been optimized.
    2. replace the holder with the optimized rule
    3. remove all holders, then optimize the rule again
    4. if the optimized rule is a recursive rule, force it to generate `CachedRule`.

---
+++ Parakeet/RuleOptimizer.NoneOptimizedRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ara3D.Parakeet
+{
+    public partial class RuleOptimizer
+    {
+        private class NoneOptimizedRule : Rule
+        {
+            public readonly Rule Rule;
+            public override IReadOnlyList<Rule> Children => new[] { Rule };
+
+            public NoneOptimizedRule(Rule rule)
+                => Rule = rule;
+
+            protected override ParserState MatchImplementation(ParserState state)
+                => throw new NotImplementedException();
+
+            public override bool Equals(object obj)
+                => obj is NoneOptimizedRule opt && Rule == opt.Rule;
+
+            public override int GetHashCode() => Hash(Rule);
+        }
+    }
+}
