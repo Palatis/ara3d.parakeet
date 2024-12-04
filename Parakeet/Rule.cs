cd1687b7b8332b66f40609bc8b23814cecca08cf
Victor Tseng <palatis@gmail.com>
2024-12-05 02:40:39 +0800

    add `typeof(OnFail)` to `OnFail.GetHashCode()`
    also hash the `Type` to get a more stable and unique hash.

--- Parakeet/Rule.cs
+++ Parakeet/Rule.cs
@@ -747,7 +747,7 @@ namespace Ara3D.Parakeet
             => obj is OnFail rec && RecoveryRule.Equals(rec.RecoveryRule);
         
         public override int GetHashCode() 
-            => Hash(RecoveryRule);
+            => Hash(typeof(OnFail), RecoveryRule);
     }
 
     /// <summary>
