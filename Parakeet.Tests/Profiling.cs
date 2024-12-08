ba19e30ce44219f8ed5c2efacaffe5835ad5a1bc
Victor Tseng <palatis@gmail.com>
2024-12-09 01:47:18 +0800

    [Tests] add a Main program for profiling

---
+++ Parakeet.Tests/Profiling.cs
@@ -0,0 +1,12 @@
+﻿using Ara3D.Parakeet.Grammars;
+
+namespace Ara3D.Parakeet.Tests
+{
+    internal static class Profiling
+    {
+        public static void Main(params string[] args)
+        {
+            CSharpTests.TargetedTest("namespace A { class B {} class C {} }", nameof(CSharpGrammar.NamespaceDeclaration));
+        }
+    }
+}
